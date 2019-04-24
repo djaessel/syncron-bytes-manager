f0d937decd57320f655eac3eeaa905e3563018c5
J.SYS <hansjuergoe@gmail.com>
2019-04-24 17:27:20 +0200

    added fixtures class + added ADMIN_ROLE to admin user

--- _tools/sql/admin.sql
+++ _tools/sql/admin.sql
@@ -5,7 +5,7 @@
 -- Server version	10.3.14-MariaDB-1
 
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'admin@syncronbytes-mgr.ddns.net','{}','$argon2i$v=19$m=1024,t=2,p=2$MDkueXpHZ1BleVJDMW1pTA$/MrGYy8oPa2Hiq1p+lvk0yanDsKpYtfF+0ys6SdZAO0');
+INSERT INTO `user` VALUES (1,'johsys@syncronbytes-mgr.ddns.net','["ROLE_ADMIN"]','$argon2i$v=19$m=1024,t=2,p=2$MDkueXpHZ1BleVJDMW1pTA$/MrGYy8oPa2Hiq1p+lvk0yanDsKpYtfF+0ys6SdZAO0');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dump completed on 2019-04-24 16:40:27
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class AppFixtures extends Fixture
+{
+    private $encoder;
+
+    /**
+     * AppFixtures constructor.
+     * @param UserPasswordEncoderInterface $encoder
+     */
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $user = new User();
+        $user->setEmail('admin@test.local');
+
+        $password = $this->encoder->encodePassword($user, 'pass_1234');
+        $user->setPassword($password);
+
+        $user->setRoles(array('ROLE_SUPER_ADMIN'));
+        
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
