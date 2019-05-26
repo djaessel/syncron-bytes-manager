7a796ea847ab0b74a8f05b9f0b41013ffd870c8e
J.SYS <hansjuergoe@gmail.com>
2019-05-26 05:44:29 +0200

    navigation real data + text color fix + admin account sql

--- _tools/sql/admin.sql
+++ _tools/sql/admin.sql
@@ -5,7 +5,13 @@
 -- Server version	10.3.14-MariaDB-1
 
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'johsys@syncronbytes-mgr.ddns.net','["ROLE_ADMIN"]','$argon2i$v=19$m=1024,t=2,p=2$MDkueXpHZ1BleVJDMW1pTA$/MrGYy8oPa2Hiq1p+lvk0yanDsKpYtfF+0ys6SdZAO0');
+
+INSERT INTO `user` VALUES (1,'degabnoti3263@syncronbytes-mgr.jsys','["ROLE_ADMIN"]','$argon2i$v=19$m=1024,t=2,p=2$MDkueXpHZ1BleVJDMW1pTA$/MrGYy8oPa2Hiq1p+lvk0yanDsKpYtfF+0ys6SdZAO0')
+ON DUPLICATE KEY UPDATE email = 'degabnoti3263@syncronbytes-mgr.jsys', roles = '["ROLE_ADMIN"]', password = '$argon2i$v=19$m=1024,t=2,p=2$MDkueXpHZ1BleVJDMW1pTA$/MrGYy8oPa2Hiq1p+lvk0yanDsKpYtfF+0ys6SdZAO0';
+
+INSERT INTO `user` VALUES (2,'axguestgrande@syncronbytes-mgr.jsys','["ROLE_ADMIN"]','$argon2i$v=19$m=1024,t=2,p=2$NTBReDZmSS5OMXk0Z1l6OA$M9nTOuenLrZM2Xi+iPS1lifXhcOmeNApaO/8+QmL1DY')
+ON DUPLICATE KEY UPDATE email = 'axguestgrande@syncronbytes-mgr.jsys', roles = '["ROLE_ADMIN"]', password = '$argon2i$v=19$m=1024,t=2,p=2$NTBReDZmSS5OMXk0Z1l6OA$M9nTOuenLrZM2Xi+iPS1lifXhcOmeNApaO/8+QmL1DY';
+
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 
 -- Dump completed on 2019-04-24 16:40:27
