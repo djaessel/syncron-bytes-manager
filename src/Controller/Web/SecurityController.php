d3852acd11bdc7ddf5b09e4297f31ff261a298bc
J.SYS <hansjuergoe@gmail.com>
2019-06-25 21:35:42 +0200

    added registration page (WIP)

--- src/Controller/Web/SecurityController.php
+++ src/Controller/Web/SecurityController.php
@@ -6,6 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
 
 class SecurityController extends AbstractController
 {
@@ -21,10 +22,43 @@ class SecurityController extends AbstractController
         // last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        return $this->render(
+			'security/login.html.twig',
+			array(
+				'last_username' => $lastUsername,
+				'error' => $error
+			)
+		);
     }
-
-    /**
+	
+	/**
+	 * @Route("/register", name="user_register")
+	 */
+	public function register(Request $request): Response
+	{
+		// create form here
+		
+		// $form->handleRequest($request);
+		
+		// handle / generate possible error messages here later
+		$error = null;
+		
+		//if ($form->isValid()) {
+		//	// save new user data
+		//	
+		//	// redirect to login page
+		//}
+		
+		return $this->render(
+			'security/register.html.twig',
+			array(
+				//'form' => $form->createView(),
+				'error' => $error,
+			)
+		);
+	}
+	
+	/**
      * @Route("/logout", name="app_logout")
      */
     public function logout()
