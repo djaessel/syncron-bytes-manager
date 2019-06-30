0f4f188b115366d1dc17f48e0622e70219b7334f
J.SYS <hansjuergoe@gmail.com>
2019-06-30 15:20:41 +0200

    symfony update to 4.2.10 + user activation web + minor route fixes

--- src/Controller/Web/BaseController.php
+++ src/Controller/Web/BaseController.php
@@ -3,16 +3,26 @@
 namespace App\Controller\Web;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class BaseController extends AbstractController
 {
     /**
      * @Route("/web/base", name="web_base")
+     * @return Response|RedirectResponse
      */
     public function index()
     {
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+
+        $user = $this->getUser();
+        $userActive = $user->getIsActive();
+        if ($userActive !== true) {
+            // user is not active
+            return $this->redirectToRoute("user_activate");
+        }
 
         return $this->render('web/base/index.html.twig', [
             'controller_name' => 'BaseController',
--- src/Controller/Web/SecurityController.php
+++ src/Controller/Web/SecurityController.php
@@ -2,7 +2,10 @@
 
 namespace App\Controller\Web;
 
+use App\Helper\JwtApiManager;
 use App\Helper\UserHelper;
+use Lexik\Bundle\JWTAuthenticationBundle\Encoder\JWTEncoderInterface;
+use Lexik\Bundle\JWTAuthenticationBundle\Services\JWTTokenManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -90,25 +93,70 @@ class SecurityController extends AbstractController
 	}
 
     /**
+     * @Route("/activate", name="user_activate")
+     * @param Request $request
+     * @param JWTTokenManagerInterface $jwtManager
+     * @param JWTEncoderInterface $jwtEncoder
+     * @return Response|RedirectResponse
+     */
+	public function activateUser(Request $request, JWTTokenManagerInterface $jwtManager, JWTEncoderInterface $jwtEncoder)
+    {
+        $activationCode = $request->request->get('activation');
+
+        if (!empty($activationCode)) {
+            $user = $this->getUser();
+            $jwtApiManager = new JwtApiManager($this->container, $jwtEncoder);
+
+            $userActivation = $jwtApiManager->getUserActivation($user, $activationCode);
+            if (!empty($userActivation)) {
+                $userActivated = $jwtApiManager->activateUser($user, $userActivation);
+                if ($userActivated) {
+                    $token = $jwtManager->create($user);
+
+                    return $this->redirectToRoute("web_base", array(
+                        "token" => $token,
+                    ));
+                }
+            }
+        }
+
+        $error = null;
+        $requestKeys = $request->request->keys();
+        if (array_search("_csrf_token", $requestKeys) !== false) {
+            $error = array(
+                "messageKey" => "activation_code_invalid",
+                "messageData" => array(),
+            );
+        }
+
+        return $this->render(
+            'security/activate.html.twig',
+            array(
+                'error' => $error,
+            )
+        );
+    }
+
+    /**
      * @return array
      */
 	private function userRegistrationErrors()
     {
         $errors = array(
             array(
-                "messageKey" => "Email is invalid",
+                "messageKey" => "email_is_invalid",
                 "messageData" => array(),
             ),
             array(
-                "messageKey" => "Password is invalid",
+                "messageKey" => "password_is_invalid",
                 "messageData" => array(),
             ),
             array(
-                "messageKey" => "Passwords are different",
+                "messageKey" => "passwords_are_different",
                 "messageData" => array(),
             ),
             array(
-                "messageKey" => "User already taken or other registration error",
+                "messageKey" => "user_taken_or_other_error",
                 "messageData" => array(),
             ),
         );
