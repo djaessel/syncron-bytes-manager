571baf00f0c029e6367ca0c1b4e66f88281dc91d
Johannes Koch <hansjuergoe@gmail.com>
2020-03-30 01:32:49 +0200

    added star controller

---
+++ src/Controller/Web/StarController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Web;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class StarController extends AbstractController
+{
+    /**
+     * @Route("/star", name="star")
+     */
+    public function index()
+    {
+		$videoPath = "/usr/local/videos/SG1_SEASON1_DISC2_SKU1_Title2.mp4";
+		$videoTitle = "Test Title 123";
+		
+        return $this->render('star/index.html.twig', [
+            'controller_name' => 'StarController',
+			'videoPath' => $videoPath,
+			'videoTitle' => $videoTitle,
+        ]);
+    }
+}
