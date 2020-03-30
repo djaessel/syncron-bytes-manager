2133c43d6d85b464dd2f42d583e17d40cbd1ca67
Johannes Koch <hansjuergoe@gmail.com>
2020-03-30 02:11:06 +0200

    video test change

--- src/Controller/Web/StarController.php
+++ src/Controller/Web/StarController.php
@@ -12,8 +12,8 @@ class StarController extends AbstractController
      */
     public function index()
     {
-		$videoPath = "/usr/local/videos/SG1_SEASON1_DISC2_SKU1_Title2.mp4";
-		$videoTitle = "Test Title 123";
+		$videoPath = "public/SG1_SEASON1_DISC2_SKU1_Title2.mp4";
+		$videoTitle = "Test";
 		
         return $this->render('star/index.html.twig', [
             'controller_name' => 'StarController',
