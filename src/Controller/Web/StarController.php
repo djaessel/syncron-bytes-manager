b30fbc577f858ac455a4c4b28533bab281e5072c
Johannes Koch <hansjuergoe@gmail.com>
2020-03-30 18:47:11 +0200

    style changes + use app.user in template

--- src/Controller/Web/StarController.php
+++ src/Controller/Web/StarController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpKernel\KernelInterface;
 
 class StarController extends AbstractController
 {
-
     /**
      * @var KernelInterface $kernel
      */
@@ -30,12 +29,9 @@ class StarController extends AbstractController
     {
       $videoFiles = $this->retrieveVideoNames();
 
-      $user = $this->getUser();
-
       return $this->render('star/index.html.twig', [
         'controller_name' => 'StarController',
         'videoFiles' => $videoFiles,
-        'user' => $user,
       ]);
     }
 
@@ -52,13 +48,10 @@ class StarController extends AbstractController
         $videoTitle = $videoFiles[$videoId];
       }
 
-      $user = $this->getUser();
-
       return $this->render('star/video.html.twig', [
         'controller_name' => 'StarController',
         'videoTitle' => $videoTitle,
         'videoId' => $videoId,
-        'user' => $user,
       ]);
     }
 
@@ -82,17 +75,4 @@ class StarController extends AbstractController
 
       return $videoFiles;
     }
-
-//    private function retrieveVideoNamesOld()
-//    {
-//      $videoPath = "./videos"; // SG1_SEASON1_DISC2_SKU1_Title2.mp4
-//      $videoFiles = scandir($videoPath);
-//
-//      $videoCount = count($videoFiles);
-//      for ($i=0; $i < $videoCount; $i++) {
-//        $videoFiles[$i] = basename($videoFiles[$i], ".mp4");
-//      }
-//
-//      return $videoFiles;
-//    }
 }
