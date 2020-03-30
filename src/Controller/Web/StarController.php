5b2dbae40eff37c8d2db7d1adf99a646e653dd35
Johannes Koch <hansjuergoe@gmail.com>
2020-03-30 16:55:26 +0200

    video star page (WIP)

--- src/Controller/Web/StarController.php
+++ src/Controller/Web/StarController.php
@@ -12,13 +12,62 @@ class StarController extends AbstractController
      */
     public function index()
     {
-		$videoPath = "/videos/SG1_SEASON1_DISC2_SKU1_Title2.mp4";
-		$videoTitle = "";
-		
-        return $this->render('star/index.html.twig', [
-			'controller_name' => 'StarController',
-			'videoPath' => $videoPath,
-			'videoTitle' => $videoTitle,
-        ]);
+      $videoFiles = retrieveVideoNames();
+
+      return $this->render('star/index.html.twig', [
+        'controller_name' => 'StarController',
+        'videoFiles' => $videoFiles;
+      ]);
+    }
+
+    /**
+     * @Route("/star/video/{videoId}", name="star-video")
+     * @param int $videoId
+     */
+    public function starVideo($videoId)
+    {
+      $videoFiles = retrieveVideoNames();
+
+      $videoTitle = "";
+      if (array_key_exists($videoFiles, $videoId)) {
+        $videoTitle = $videoFiles[$videoId];
+      }
+
+      return $this->render('star/video.html.twig', [
+        'controller_name' => 'StarController',
+        'videoTitle' => $videoTitle,
+        'videoId' => $videoId,
+      ]);
+    }
+
+    private function retrieveVideoNames()
+    {
+      $projectRoot = $this->get('kernel')->getProjectDir();
+      $videoFilesNamePath = $projectRoot . "/_tools/videoFileNames.csv";
+      $videoFiles = array();
+
+      if (($handle = fopen($videoFilesNamePath, "r")) !== FALSE) {
+          while (($data = fgetcsv($handle, 1000, ";")) !== FALSE) {
+            if (count($data) > 1) {
+              $videoFiles[$data[0]] = $data[1];
+            }
+          }
+          fclose($handle);
+      }
+
+      return $videoFiles;
+    }
+
+    private function retrieveVideoNamesOld()
+    {
+      $videoPath = "./videos"; // SG1_SEASON1_DISC2_SKU1_Title2.mp4
+      $videoFiles = scandir($videoPath);
+
+      $videoCount = count($videoFiles);
+      for ($i=0; $i < $videoCount; $i++) {
+        $videoFiles[$i] = basename($videoFiles[$i], ".mp4");
+      }
+
+      return $videoFiles;
     }
 }
