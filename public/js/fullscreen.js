f2523d75450999e8106540d207f4002f27aaea76
J.SYS <hansjuergoe@gmail.com>
2020-04-04 23:36:16 +0200

    videoButton test

--- public/js/fullscreen.js
+++ public/js/fullscreen.js
@@ -1,30 +1,31 @@
 /**
  * created by https://www.sitepoint.com/html5-full-screen-api/
  */
+
+ var pfx = ["webkit", "moz", "ms", "o", ""];
+
+ function RunPrefixMethod(obj, method) {
+   var p = 0, m, t;
+   while (p < pfx.length && !obj[m]) {
+     m = method;
+     if (pfx[p] == "") {
+       m = m.substr(0, 1).toLowerCase() + m.substr(1);
+     }
+     m = pfx[p] + m;
+     t = typeof obj[m];
+     if (t != "undefined") {
+       pfx = [pfx[p]];
+       return (t == "function" ? obj[m]() : obj[m]);
+     }
+     p++;
+   }
+ }
+
 $(function () {
-
-  var pfx = ["webkit", "moz", "ms", "o", ""];
-
-  function RunPrefixMethod(obj, method) {
-    var p = 0, m, t;
-    while (p < pfx.length && !obj[m]) {
-      m = method;
-      if (pfx[p] == "") {
-        m = m.substr(0, 1).toLowerCase() + m.substr(1);
-      }
-      m = pfx[p] + m;
-      t = typeof obj[m];
-      if (t != "undefined") {
-        pfx = [pfx[p]];
-        return (t == "function" ? obj[m]() : obj[m]);
-      }
-      p++;
-    }
-  }
 
   $('.videoButton').on('click', function(e) {
     e.preventDefault();
-    
+
     if (RunPrefixMethod(document, "FullScreen") || RunPrefixMethod(document, "IsFullScreen")) {
       RunPrefixMethod(document, "CancelFullScreen");
     }
