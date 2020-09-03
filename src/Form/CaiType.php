42f884189a6ab23d9a4d6d3ddf2e824e5c57db72
J.SYS <hansjuergoe@gmail.com>
2020-09-03 08:45:58 +0200

    cai formtype for multiple files

--- src/Form/CaiType.php
+++ src/Form/CaiType.php
@@ -19,9 +19,18 @@ class CaiType extends AbstractType
                 // unmapped means that this field is not associated to any entity property
                 'mapped' => false,
 
+                // deprecated?
+                // 'attr' => [
+                //     'accept' => 'image/*',
+                //     'multiple' => 'multiple'
+                // ],
+
                 // make it optional so you don't have to re-upload the PDF file
                 // everytime you edit the Product details
                 'required' => false,
+
+                // for multiple images (necessary for merging)
+                'multiple' => true,
 
                 // unmapped fields can't define their validation using annotations
                 // in the associated entity, so you can use the PHP constraint classes
