ff2b27d68bd73b95116ddefe80843ef8af6a72e7
J.SYS <hansjuergoe@gmail.com>
2020-10-08 20:52:32 +0200

    star:command const fix

--- src/Command/StarImportCommand.php
+++ src/Command/StarImportCommand.php
@@ -16,7 +16,7 @@ class StarImportCommand extends BaseCommand
 {
     protected static $defaultName = 'start:import';
 
-    private const DEFAULT_PROJECT_PATH = "/tools";
+    const DEFAULT_PROJECT_PATH = "/tools";
 
     /**
      * FileValidCheckCommand constructor.
