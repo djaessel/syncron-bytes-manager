7f1e83020a0fb3130f24dbbb45bf62f2922bcf87
J.SYS <hansjuergoe@gmail.com>
2020-10-08 20:41:10 +0200

    migration for episode numbers added

---
+++ src/Migrations/Version20201008184022.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20201008184022 extends AbstractMigration
+{
+    public function getDescription() : string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE episode CHANGE number number VARCHAR(8) NOT NULL, CHANGE number_all number_all VARCHAR(8) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema) : void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE episode CHANGE number number INT NOT NULL, CHANGE number_all number_all INT DEFAULT NULL');
+    }
+}
