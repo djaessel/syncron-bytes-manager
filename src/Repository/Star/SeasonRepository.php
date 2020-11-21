8a51f492bed7ffb3b34434be9e79dcf372fc73b6
J.SYS <hansjuergoe@gmail.com>
2020-11-21 15:06:45 +0100

    repository fix (again)

--- src/Repository/Star/SeasonRepository.php
+++ src/Repository/Star/SeasonRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository\Star;
 
 use App\Entity\Star\Season;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method Season|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class SeasonRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Season::class);
     }
--- src/Repository/TransferFileRepository.php
+++ src/Repository/TransferFileRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\TransferFile;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Symfony\Bridge\Doctrine\RegistryInterface;
+use Doctrine\Common\Persistence\ManagerRegistry;
 
 /**
  * @method TransferFile|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,7 +14,7 @@ use Symfony\Bridge\Doctrine\RegistryInterface;
  */
 class TransferFileRepository extends ServiceEntityRepository
 {
-    public function __construct(RegistryInterface $registry)
+    public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, TransferFile::class);
     }
