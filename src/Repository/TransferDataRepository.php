bb9a3210d9730aed29516b404cf77c3a37d1f566
J.SYS <hansjuergoe@gmail.com>
2020-11-21 15:09:43 +0100

    and another repository fix attempt

--- src/Repository/TransferDataRepository.php
+++ src/Repository/TransferDataRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\TransferData;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method TransferData|null find($id, $lockMode = null, $lockVersion = null)
--- src/Repository/TransferFileRepository.php
+++ src/Repository/TransferFileRepository.php
@@ -4,7 +4,7 @@ namespace App\Repository;
 
 use App\Entity\TransferFile;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
-use Doctrine\Common\Persistence\ManagerRegistry;
+use Doctrine\Persistence\ManagerRegistry;
 
 /**
  * @method TransferFile|null find($id, $lockMode = null, $lockVersion = null)
